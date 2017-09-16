f00bff3b06f1ffe9ecba30a034d2cb661453d900
Alin Panaitiu <alin.p32@gmail.com>
2017-09-16 22:27:53 +0300

    Update

--- setup.py
+++ setup.py
@@ -1,21 +1,57 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+with open('subfix/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+with open('README.md', 'rb') as f:
+    readme = f.read().decode('utf-8')
+
+REQUIRES = [
+    'fire',
+    'pysrt',
+    'daiquiri',
+]
 
 setup(
     name='subfix',
-    version='0.1',
+    version=version,
     description='Subtitle fixer',
-    url='https://github.com/alin23/subfix',
+    long_description=readme,
     author='Alin Panaitiu',
     author_email='alin.p32@gmail.com',
-    license='MIT',
-    packages=['subfix'],
-    install_requires=[
-        'fire',
-        'pysrt',
-        'daiquiri'
+    maintainer='Alin Panaitiu',
+    maintainer_email='alin.p32@gmail.com',
+    url='https://github.com/alin23/subfix',
+    license='MIT/Apache-2.0',
+
+    keywords=[
+        '',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     entry_points={
         'console_scripts': ['subfix = subfix.subfix:main']
     },
-    zip_safe=False
+
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+
+    packages=find_packages(),
 )
