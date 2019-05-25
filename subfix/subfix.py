d27b7beb197fb5cd42964c68088221afa341e93f
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-05-25 14:59:19 +0300

    Add backup

--- subfix/subfix.py
+++ subfix/subfix.py
@@ -8,17 +8,19 @@ import pysrt
 from .log import logger
 
 
-class SubtitleFixer():
+class SubtitleFixer:
     """Utility for fixing subtitles.
 
     Attributes:
         dir (str, optional): Default path to work on
     """
 
-    MOVIE_EXTS = ['*.mkv', '*.avi', '*.mp4']
-    SUB_EXTS = ['*.srt', '*.sub']
+    BACKUP_EXT = ".bkp"
+    MOVIE_EXTS = ["*.mkv", "*.avi", "*.mp4"]
+    SUB_EXTS = ["*.srt", "*.sub"]
+    SUB_BKP_EXTS = [f"{ext}{BACKUP_EXT}" for ext in SUB_EXTS]
 
-    def __init__(self, dir='.'):
+    def __init__(self, dir="."):
         super().__init__()
         self.dir = Path(dir)
 
@@ -33,7 +35,13 @@ class SubtitleFixer():
         """
 
         dir = Path(dir or self.dir)
-        return sum((sorted(dir.glob(ext), key=lambda p: str(p).lower()) for ext in self.MOVIE_EXTS), [])
+        return sum(
+            (
+                sorted(dir.glob(ext), key=lambda p: str(p).lower())
+                for ext in self.MOVIE_EXTS
+            ),
+            [],
+        )
 
     def subtitles(self, dir=None):
         """Get subtitle paths.
@@ -46,9 +54,43 @@ class SubtitleFixer():
         """
 
         dir = Path(dir or self.dir)
-        return sum((sorted(dir.glob(ext), key=lambda p: str(p).lower()) for ext in self.SUB_EXTS), [])
+        return sum(
+            (
+                sorted(dir.glob(ext), key=lambda p: str(p).lower())
+                for ext in self.SUB_EXTS
+            ),
+            [],
+        )
 
-    def recode(self, dir=None, subtitle=None, source='windows-1250', target='utf-8', start_index=0, stop_index=None):
+    def subtitle_backups(self, dir=None):
+        """Get subtitle backup paths.
+
+        Args:
+            dir (str, optional): Folder where to search for backups
+
+        Returns:
+            list: List of subtitle backup paths
+        """
+
+        dir = Path(dir or self.dir)
+        return sum(
+            (
+                sorted(dir.glob(ext), key=lambda p: str(p).lower())
+                for ext in self.SUB_BKP_EXTS
+            ),
+            [],
+        )
+
+    def recode(
+        self,
+        dir=None,
+        subtitle=None,
+        source="windows-1250",
+        target="utf-8",
+        start_index=0,
+        stop_index=None,
+        force=False,
+    ):
         """Re-encode subtitle using a different encoding.
 
         Args:
@@ -69,12 +111,13 @@ class SubtitleFixer():
             try:
                 text = sub.read_text(encoding=source)
             except Exception:
-                logger.error(f'{sub.name} is not encoded as {source}')
+                logger.error(f"{sub.name} is not encoded as {source}")
             else:
+                self.backup(sub, force)
                 sub.write_bytes(text.encode(target))
-                logger.info(f'Re-encoded {sub.name} as {target}')
+                logger.info(f"Re-encoded {sub.name} as {target}")
 
-    def rename(self, dir=None, start_index=0, stop_index=None):
+    def rename(self, dir=None, start_index=0, stop_index=None, force=False):
         """Rename subtitles with the same name as the movie files.
 
         Args:
@@ -84,13 +127,38 @@ class SubtitleFixer():
         """
 
         dir = Path(dir or self.dir)
-        for sub, movie in list(zip(self.subtitles(dir), self.movies(dir)))[start_index:stop_index]:
+        for sub, movie in list(zip(self.subtitles(dir), self.movies(dir)))[
+            start_index:stop_index
+        ]:
             newsub = sub.with_name(movie.stem + sub.suffix)
             if sub != newsub:
+                self.backup(sub, force)
                 sub.replace(newsub)
-                logger.info(f'Renamed {sub.name} to {newsub.name}')
+                logger.info(f"Renamed {sub.name} to {newsub.name}")
 
-    def shift(self, dir=None, subtitle=None, start_index=0, stop_index=None, **by):
+    def restore(self, dir=None, subtitle=None, start_index=0, stop_index=None):
+        """Restore subtitle backups.
+
+        Args:
+            dir (str, optional): Folder where to apply the restore
+            subtitle (str, optional): Subtitle to restore
+            start_index (int, optional): First index of subtitle list
+            stop_index (None, optional): Last index of subtitle list
+        """
+
+        dir = Path(dir or self.dir)
+        if subtitle:
+            subtitles = [Path(subtitle)]
+        else:
+            subtitles = self.subtitle_backups(dir)
+        for sub in subtitles[start_index:stop_index]:
+            newsub = sub.with_suffix("")
+            sub.replace(newsub)
+            logger.info(f"Restored {sub.name} as {newsub.name}")
+
+    def shift(
+        self, dir=None, subtitle=None, start_index=0, stop_index=None, force=True, **by
+    ):
         """Shift subtitles by a ratio or an offset.
 
         Args:
@@ -107,10 +175,19 @@ class SubtitleFixer():
             subtitles = [subtitle]
 
         for subtitle in subtitles:
+            self.backup(subtitle, force)
             sub = pysrt.open(subtitle)
             sub.shift(**by)
             sub.save(subtitle)
-            logger.info(f'Shifted {subtitle} by {by.items()}')
+            logger.info(f"Shifted {subtitle} by {by.items()}")
+
+    def backup(self, subtitle, force=False):
+        bkp = subtitle.with_suffix(f"{subtitle.suffix}{self.BACKUP_EXT}")
+        if bkp.exists() and not force:
+            logger.warning("Skipping backup of %s as it already exists", subtitle)
+            return
+        bkp.write_bytes(subtitle.read_bytes())
+        logger.info("Backed up %s as %s", subtitle, bkp)
 
     def fix(self, dir=None, start_index=0, stop_index=None):
         """Attempt to apply all possible fixes:
@@ -134,5 +211,5 @@ def main():
     fire.Fire(SubtitleFixer)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
